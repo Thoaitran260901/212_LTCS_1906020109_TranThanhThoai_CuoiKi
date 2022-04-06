68f41a94d012a024090920529b4f50d2923b7be7
Thoaitran260901 <tranthanhthoai260901@gmail.com>
2022-04-06 21:54:24 +0700

    Add project files.

---
+++ Tetris/Models/ScoreModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris.Models
+{
+    internal class ScoreModel
+    {
+        public string NickName { get; set; }
+        public int Score { get; set; }
+        
+        public ScoreModel()
+        {
+
+        }
+
+    }
+}
